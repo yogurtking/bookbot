1749ee923b4b7cd7b35bcee8abf40a7f2b461794
Yogurtking <Yogurtking@gmail.com>
2025-01-12 14:18:56 -0500

    split up the functions to be cleaner and more in line with the tutorial

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 
 def main():
     word_count = 0
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        for i in range(len(words)):
-            word_count += 1
-
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_word_count(text)
     print(f"total words in THIS BOOK: {word_count}")
 
 
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+    # print(file_contents)
+    
+# get the text of the book and return it
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
 
